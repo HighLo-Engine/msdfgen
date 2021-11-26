27a46013d18fd92c3148722f54f3b4f840aa7851
Can Karka <53074877+Cankar001@users.noreply.github.com>
2021-11-26 15:51:37 +0100

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -4,7 +4,7 @@ project "msdfgen"
 	kind "StaticLib"
 	language "C++"
 	cppdialect "C++17"
-    staticruntime "On"
+    	staticruntime "off"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
