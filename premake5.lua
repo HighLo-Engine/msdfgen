b152c1f6e5cb3fda21c73dcc15413ad6d10f1a3f
Can Karka <53074877+Cankar001@users.noreply.github.com>
2021-12-20 14:39:22 +0100

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -40,12 +40,35 @@ project "msdfgen"
 	filter "system:windows"
 		systemversion "latest"
 
-	filter "configurations:Debug"
+	filter "configurations:Debug-OpenGL"
 		runtime "Debug"
 		symbols "on"
 
-	filter "configurations:Release"
+	filter "configurations:Debug-Vulkan"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Debug-DX11"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Debug-DX12"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release-OpenGL"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Release-Vulkan"
 		runtime "Release"
 		optimize "on"
 		
-		
+	filter "configurations:Release-DX11"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Release-DX12"
+		runtime "Release"
+		optimize "on"
+
